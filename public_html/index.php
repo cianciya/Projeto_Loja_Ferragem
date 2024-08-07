225d1e396bb57225f1651bafeb5267e7df60f907
cianciya <yagogarsoncianci@gmail.com>
2024-08-07 09:20:18 -0300

    Modificação das rotas / Testes feitos pelo postman

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,22 @@ require_once '../src/controller/ProdutoController.php';
 // Inicializando o controlador de produtos com a conexão ao banco de dados
 $produtoController = new ProdutoController($pdo);
 
+// Função para enviar uma resposta JSON
+function sendJsonResponse($data) {
+    header('Content-Type: application/json');
+    echo json_encode($data);
+    exit();
+}
+
+// Função para verificar se o JSON foi decodificado corretamente
+function getJsonData() {
+    $data = json_decode(file_get_contents('php://input'), true);
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        sendJsonResponse(['message' => 'Erro ao decodificar JSON']);
+    }
+    return $data;
+}
+
 // Verificando o método HTTP da requisição e executando a ação correspondente
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
@@ -13,32 +29,52 @@ switch ($_SERVER['REQUEST_METHOD']) {
         if (isset($_GET['id'])) {
             // Se um ID for fornecido, obter o produto por ID
             $produto = $produtoController->obterProdutoPorId($_GET['id']);
-            echo json_encode($produto); // Retornar o produto em formato JSON
+            sendJsonResponse($produto); // Retornar o produto em formato JSON
         } else {
             // Se não, listar todos os produtos
             $produtos = $produtoController->listarProdutos();
-            echo json_encode($produtos); // Retornar a lista de produtos em formato JSON
+            sendJsonResponse($produtos); // Retornar a lista de produtos em formato JSON
         }
         break;
+    
     case 'POST':
         // Caso seja uma requisição POST
-        $data = json_decode(file_get_contents('php://input'), true); // Decodificar o JSON enviado
-        $produtoController->adicionarProduto($data['nome'], $data['descricao'], $data['preco'], $data['quantidade'], $data['categoria_id'], $data['fornecedor_id']);
-        echo json_encode(['message' => 'Produto adicionado com sucesso!']); // Mensagem de sucesso
+        $data = getJsonData();
+
+        // Verificando se todos os campos necessários estão presentes
+        if (isset($data['nome_produto'], $data['descricao_produto'], $data['preco_produto'], $data['quantidade_estoque'], $data['id_categoria'], $data['id_fornecedor'])) {
+            $produtoController->adicionarProduto($data['nome_produto'], $data['descricao_produto'], $data['preco_produto'], $data['quantidade_estoque'], $data['id_categoria'], $data['id_fornecedor']);
+            sendJsonResponse(['message' => 'Produto adicionado com sucesso!']);
+        } else {
+            sendJsonResponse(['message' => 'Dados do produto incompletos']);
+        }
         break;
+    
     case 'PUT':
         // Caso seja uma requisição PUT
-        $data = json_decode(file_get_contents('php://input'), true); // Decodificar o JSON enviado
-        $produtoController->atualizarProduto($data['id'], $data['nome'], $data['descricao'], $data['preco'], $data['quantidade'], $data['categoria_id'], $data['fornecedor_id']);
-        echo json_encode(['message' => 'Produto atualizado com sucesso!']); // Mensagem de sucesso
+        $data = getJsonData();
+
+        // Verificando se todos os campos necessários estão presentes
+        if (isset($data['id'], $data['nome_produto'], $data['descricao_produto'], $data['preco_produto'], $data['quantidade_estoque'], $data['id_categoria'], $data['id_fornecedor'])) {
+            $produtoController->atualizarProduto($data['id'], $data['nome_produto'], $data['descricao_produto'], $data['preco_produto'], $data['quantidade_estoque'], $data['id_categoria'], $data['id_fornecedor']);
+            sendJsonResponse(['message' => 'Produto atualizado com sucesso!']);
+        } else {
+            sendJsonResponse(['message' => 'Dados do produto incompletos']);
+        }
         break;
+    
     case 'DELETE':
         // Caso seja uma requisição DELETE
         if (isset($_GET['id'])) {
-            // Se um ID for fornecido, deletar o produto
             $produtoController->deletarProduto($_GET['id']);
-            echo json_encode(['message' => 'Produto deletado com sucesso!']); // Mensagem de sucesso
+            sendJsonResponse(['message' => 'Produto deletado com sucesso!']);
+        } else {
+            sendJsonResponse(['message' => 'ID do produto não fornecido']);
         }
         break;
+    
+    default:
+        sendJsonResponse(['message' => 'Método HTTP não permitido']);
 }
+
 ?>
