9734b0150723ef5dea0746520547f04ffa07ebef
edyguilherme1 <edyguilherme@gmail.com>
2024-08-15 22:39:04 -0300

    Adicionado controler.php e jquery para requisicao
    Adicionado controler.php e jquery para requisicao no javascript

---
+++ public_html/controller.php
@@ -0,0 +1,18 @@
+<?php
+    echo "teste";
+    // header('Access-Control-Allow-Origin: *');
+    // header('Content-Type: application/json');
+
+    // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //     // Tratar a solicitação POST
+    //     // $nome = $_POST['nome'];
+    //     // $data = array('produto' => $_POST['nome'], 'produto2' => $_POST['nome']);
+    //     // echo json_encode($data);
+    //     echo json_encode("Entrou no Post com sucesso");
+    //     // ...
+    // } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    //     // Tratar a solicitação GET
+    //     echo json_encode("Entrou no Get com sucesso");
+    //     // ...
+    // }
+?>
--- public_html/js/event.js
+++ public_html/js/event.js
@@ -1,8 +1,29 @@
+function BuscaDados(){
+  // $.get('http://localhost/www/controller.php', function(resposta) {
+    // alert(resposta);
+  // });
+}
+
+function EnviaDados(texto){
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost/www/controller.php',
+    success: function(resposta) {
+      // alert(resposta);
+      // Tratar a resposta aqui
+    },
+    error: function(xhr, status, erro) {
+      // alert('Erro ao obter dados:', erro);
+    }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   const addToCartButtons = document.querySelectorAll('.add-to-cart');
   addToCartButtons.forEach(function(button) {
     button.addEventListener('click', function() {
-      alert('Produto adicionado ao carrinho!');
+      alert('Adicionado no carrinho com sucesso');
+      //BuscaDados();
     });
   });
 });
